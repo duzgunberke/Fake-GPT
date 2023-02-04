8d8b0b55fdcbf668cd70031d51b2b9bcea38de70
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-02-05 02:33:50 +0300

    refactor(client): apı url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@
    loader(messageDiv);
   
    //fetch data from server -> bot's response
-   const response = await fetch('http://localhost:5000',{
+   const response = await fetch('https://fake-gpt.onrender.com',{
     method: 'POST',
     headers:{
       'Content-Type': 'application/json'
